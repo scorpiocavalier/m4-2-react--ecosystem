6134d6fc8e064f4a6a257a76e93f0f13cccc4dab
Scott Morin <scottanthonymorin@gmail.com>
2020-07-20 14:53:04 -0400

    remove solution

--- __solution/src/components/Paragraph.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-
-export default styled.p`
-  font-size: 21px;
-  margin-bottom: 32px;
-`;
--- __solution/src/theme.js
+++
@@ -1,4 +0,0 @@
-export const COLORS = {
-  primary: 'hsl(259deg, 100%, 44%)',
-  secondary: 'hsl(333deg, 100%, 50%)',
-};
